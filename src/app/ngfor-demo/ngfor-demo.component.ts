df49f588b8c01fec4e2a4aad3a862f4fe47d6386
Sarthak-Mokashi <69505673+Sarthak-Mokashi@users.noreply.github.com>
2021-01-12 20:05:46 +0530

    initial init

--- src/app/ngfor-demo/ngfor-demo.component.ts
+++ src/app/ngfor-demo/ngfor-demo.component.ts
@@ -8,34 +8,27 @@ import { DataServiceService } from '../data-service.service';
 })
 export class NgforDemoComponent implements OnInit {
   
-  
+  countryIndex=0;
+  showData = false;
+  tableHeaders: any = [];
+  stateTableHeaders: any = [];
+  countries :any = [];
 
  // constructor(private dataService : DataServiceService) { }              // If calling Service's Method only from this Class
 
   constructor(public dataService : DataServiceService) { }                 // If calling Service's Method directly from template
 
-  showData = false;
-
-  public toggle(event: any)
-  {
-    console.log('event id is'+event);
-    this.showData = !this.showData;
-
-  }
-
-  
-  tableHeaders: any = [];
-  stateTableHeaders: any = [];
-  countries :any = [];
-  
   ngOnInit(): void {
-
     console.log(this.dataService.getCountries());
     this.tableHeaders = this.dataService.getCountriesHeaders();
     this.stateTableHeaders = this.dataService.getStateHeaders();
     this.countries = this.dataService.getCountries();
-
     }
 
+  public toggle(event: any,index:number)
+  {
+    console.log('event id is'+event);
+    this.showData = !this.showData;
+  this.countryIndex=index
   }
-
+  }
